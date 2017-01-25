bed7858d17b70677430b9170c358abe3a71e572e
Sty BL <stysdaman@gmail.com>
2017-01-25 23:33:07 +0200

    Implemented module repo interface.

---
+++ EvolvedAPI/Models/IModuleRepository.cs
@@ -0,0 +1,14 @@
+﻿namespace EvolvedAPI.Models
+{
+    using System;
+    using System.Collections.Generic;
+
+    public interface IModuleRepository
+    {
+        void Add(Module module);
+        IEnumerable<Module> GetAll();
+        Module Find(int id);
+        Module Remove(int id);
+        void Update(Module module);
+    }
+}
