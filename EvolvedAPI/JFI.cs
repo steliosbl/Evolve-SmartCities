21e06767d5e05c8f98beeb4e7d0f3908d2f2c4b8
Sty BL <stysdaman@gmail.com>
2017-01-26 13:14:48 +0200

    Added components (MySQLClient, SDB, JFI).

---
+++ EvolvedAPI/JFI.cs
@@ -0,0 +1,20 @@
+﻿namespace DistributionCommon
+{
+    using System;
+    using System.IO;
+
+    public static class JFI
+    {
+        public static T GetObject<T>(string filename)
+        {
+            try
+            {
+                return Newtonsoft.Json.JsonConvert.DeserializeObject<T>(File.ReadAllText(filename));
+            }
+            catch (IOException)
+            {
+                return default(T);
+            }
+        }
+    }
+}
